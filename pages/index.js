6620875f936254ab357a86ca7ff51a864cda0215
Andrew Moses <andrewmosesdrive@gmail.com>
2022-01-23 11:41:52 -0700

    added url for og img

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,10 @@ export default function Home() {
         <meta property="og:title" content="amwriteswords" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.amwriteswords.net" />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://www.amwriteswords.net/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FconversationsPic.5b673628.jpg&w=1920&q=75"
+        />
         <meta name="description" content="Home page for Andrew Moses, author" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
